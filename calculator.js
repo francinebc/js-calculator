ed32538babf4997f59daede7b75fb4dcae776a05
nzbenji <benlittle53@gmail.com>
2018-12-19 11:07:21 +1300

    calculation is now working

--- calculator.js
+++ calculator.js
@@ -1,4 +1,12 @@
+var state = {
+    factors: [
+        [ '0', 'number' ] 
+    ],
+    evalulated: false
+}
+
 const buttons = document.querySelector('.buttons');
+const displayValue = document.querySelector('.displayView');
 
 buttons.addEventListener('click', btnClick)
 
@@ -6,21 +14,57 @@ function btnClick(event) {
     const target = event.target;
 
     if(target.classList[1] === 'number') {
-        console.log('is number')
+        addNumbers(target.value)
     }
     
     if(target.classList[1] === 'operator') {
-        console.log('operator')
+        addOperator(target.value)
     }
 
     if(target.classList[1] === 'equals') {
-        console.log('equals')
+        evalulate()
     }
 
     if(target.classList[1] === 'clear') {
         console.log('clear')
     }
 
-    if(target.classList[1] === 'decimal') console.log('decimal')
+    if(target.classList[1] === 'decimal'){
+        addDecimal(target.value)
+    }
+    console.log(state)
+    display()
 }
+
+function addNumbers(number) {
+    state.factors.push([number, 'number'])
+}
+
+function addOperator(operator){
+    state.factors.push([operator, 'operator'])
+}
+
+function addDecimal(decimal) {
+    state.factors.push([decimal, 'decimal'])
+}
+
+function evalulate() {
+    
+    state.factors = [[eval(toString()), 'number' ]]
+}
+
+function toString() {
+    let string = '';
+    state.factors.forEach(value => {
+        let val = value[0] 
+        string += val
+    });
+    return string;
+}
+
+function display() {
+    displayValue.value = toString()
+}
+
+
 
