16cabdd9b4014246d4f1e4bb38321dc1c76e77a1
nzbenji <benlittle53@gmail.com>
2018-12-19 10:34:41 +1300

    pseudocode added

--- calculator.js
+++ calculator.js
@@ -1,1 +1,26 @@
-console.log("hello");
+const buttons = document.querySelector('.buttons');
+
+buttons.addEventListener('click', btnClick)
+
+function btnClick(event) {
+    const target = event.target;
+
+    if(target.classList[1] === 'number') {
+        console.log('is number')
+    }
+    
+    if(target.classList[1] === 'operator') {
+        console.log('operator')
+    }
+
+    if(target.classList[1] === 'equals') {
+        console.log('equals')
+    }
+
+    if(target.classList[1] === 'clear') {
+        console.log('clear')
+    }
+
+    if(target.classList[1] === 'decimal') console.log('decimal')
+}
+
---
+++ pseudocode.md
@@ -0,0 +1,11 @@
+Set up event listeners 
+new event listener method 
+check class's if operator/number/equals/dot/clear
+in case of operator store in array and render display value
+    if there is an operator return
+in case of number store in array and render display value
+    if recently eval putting in new number clear screen
+in case of dot store in array and render display value
+    if there is an decimal return if recently eval clear screen
+in case of clear delete and render display value
+case of equals perform eval on array and render display
